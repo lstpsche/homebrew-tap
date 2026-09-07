78e4b24d1962e62a1279ca37f8c035f7462a8d33
Nikita Shkoda <lstpsche@gmail.com>
2026-09-07 18:33:02 +0300

    chore: update telegram-mcp to 1.0.0

--- Formula/telegram-mcp.rb
+++ Formula/telegram-mcp.rb
@@ -10,23 +10,23 @@ class TelegramMcp < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/lstpsche/telegram-mcp/releases/download/v0.8.0/telegram-mcp-0.8.0-darwin-arm64.zip"
-      sha256 "c283df93218c19170cf239e50f66dba0f85dc6586faf6a7646f7d60ae5b168ea"
+      url "https://github.com/lstpsche/telegram-mcp/releases/download/v1.0.0/telegram-mcp-1.0.0-darwin-arm64.zip"
+      sha256 "eef275eb1e18910dea6a8f0809ba922e79f8fd1bb358df5023421f285ab478c0"
     end
     on_intel do
-      url "https://github.com/lstpsche/telegram-mcp/releases/download/v0.8.0/telegram-mcp-0.8.0-darwin-amd64.zip"
-      sha256 "110cc048be85845349b643e7512ad076a4c872acadc704ca94c9bbf2ffb4865d"
+      url "https://github.com/lstpsche/telegram-mcp/releases/download/v1.0.0/telegram-mcp-1.0.0-darwin-amd64.zip"
+      sha256 "c96621321d0a1d5c6615ba7cfc8ac68b39c95b98e7034252482127d0e5e8bbce"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/lstpsche/telegram-mcp/releases/download/v0.8.0/telegram-mcp-0.8.0-linux-arm64.zip"
-      sha256 "0838db673a825f394424e72c92df3de64292c96d47a5e301e0ea50bcee705b2f"
+      url "https://github.com/lstpsche/telegram-mcp/releases/download/v1.0.0/telegram-mcp-1.0.0-linux-arm64.zip"
+      sha256 "2a33459e3db6cdec8ee71a18dfd1ba2d94fe6e9e0579b5e0346fe7203ba54d8d"
     end
     on_intel do
-      url "https://github.com/lstpsche/telegram-mcp/releases/download/v0.8.0/telegram-mcp-0.8.0-linux-amd64.zip"
-      sha256 "bc52af98ce9d7216367dd0c923b5a635b12784608e90405682212a711d7adfd7"
+      url "https://github.com/lstpsche/telegram-mcp/releases/download/v1.0.0/telegram-mcp-1.0.0-linux-amd64.zip"
+      sha256 "0559cfd56c8db31844914e5cabe4a5c39ce2d928a8623a7f0a151fef0415126d"
     end
   end
 
